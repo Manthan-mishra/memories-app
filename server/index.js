d740c097a1639620835dfb04ce021eb616b0378c
Manthan-mishra <manthan9481147615@gmail.com>
2023-05-16 20:51:44 +0530

    connecting the server to mongodb database

--- server/index.js
+++ server/index.js
@@ -7,3 +7,19 @@ import mongoose from "mongoose";
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://manthan_mishra25:manthan123@cluster0.ele5pmv.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(error.message));
+
+// mongoose.set("useFindAndModify", false);
