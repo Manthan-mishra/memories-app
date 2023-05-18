e8a4599fca3528a9ab9e3a157b12344692c32615
Manthan-mishra <manthan9481147615@gmail.com>
2023-05-18 16:55:55 +0530

    project

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
